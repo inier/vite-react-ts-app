950943f3bf139f73580cd61e74fda6fa44abc210
inier@qq.com <iniers@outlook.com>
2021-03-30 18:12:29 +0800

    fix: 增加图片静态资源

--- src/pages/page2/index.tsx
+++ src/pages/page2/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import svgImg from '@/assets/logo.svg';
+import pngImg from '@/assets/marker.png';
 
-const Page2: React.FC = () => <div>Page2</div>;
+const Page2: React.FC = () => (
+  <div>
+    <span>Page2</span>
+    <img src={svgImg} alt="" />
+    <img src={pngImg} alt="" />
+  </div>
+);
 
 export default Page2;
